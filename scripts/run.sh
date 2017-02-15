3703dc9f341de9a06ff3c941543eabdf753c239b
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-02-15 04:57:29 +0100

    colors

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo "=> run"
+COLOR_END="\e[39m"
+COLOR_BLUE="\e[34m"
+COLOR_LBLUE="\e[94m"
+COLOR_YELLOW="\e[33m"
+COLOR_LYELLOW="\e[93m"
+
+echo -e
+echo -e "[$(date --rfc-3339 seconds)] - ${COLOR_BLUE}Runing tests...$COLOR_END"
+echo -e
 
 pushd "$HOME" &>/dev/null
   pushd kuzzle-proxy &>/dev/null
