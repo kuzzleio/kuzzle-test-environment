b8272c3b5dadccd8257f422efea2abb4be5e5ab5
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-02-16 03:53:50 +0100

    travis debug

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -8,3 +8,5 @@ curl -kO "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.t
 tar -xvf "node-v$NODE_VERSION-linux-x64.tar.gz" -C /usr/local --strip-components=1
 rm "node-v$NODE_VERSION-linux-x64.tar.gz"
 ln -s /usr/local/bin/node /usr/local/bin/nodejs
+
+which node
