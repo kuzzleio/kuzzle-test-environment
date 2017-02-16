4f2663abe73771a50d3477a9a6fe97df824107b7
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-02-16 04:09:00 +0100

    travis debug

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,11 @@ set -e
 
 ELASTIC_HOST=${kuzzle_services__db__host:-localhost}
 ELASTIC_PORT=${kuzzle_services__db__port:-9200}
+if [ $TRAVIS -eq "true" ]; then
+  echo "travis"
+fi
+
+PATH="/home/travis/.rvm/gems/ruby-2.2.5/bin:/home/travis/.rvm/gems/ruby-2.2.5@global/bin:/home/travis/.rvm/rubies/ruby-2.2.5/bin:/home/travis/.rvm/bin:/home/travis/bin:/home/travis/.local/bin:/home/travis/.gimme/versions/go1.4.2.linux.amd64/bin:/usr/local/phantomjs/bin:./node_modules/.bin:/usr/local/maven-3.2.5/bin:/usr/local/clang-3.4/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
 
 
 COLOR_END="\e[39m"
