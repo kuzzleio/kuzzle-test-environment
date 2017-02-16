d4a538127131148915d30d314ca5a2c455f4ec59
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-02-16 13:08:34 +0100

    debug travis

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,6 +7,10 @@ COLOR_LBLUE="\e[94m"
 COLOR_YELLOW="\e[33m"
 COLOR_LYELLOW="\e[93m"
 
+if [[ $TRAVIS -eq "true" ]]; then
+  export PATH="/tmp/.npm-global/bin:/home/travis/.rvm/gems/ruby-2.2.5/bin:/home/travis/.rvm/gems/ruby-2.2.5@global/bin:/home/travis/.rvm/rubies/ruby-2.2.5/bin:/home/travis/.rvm/bin:/home/travis/bin:/home/travis/.local/bin:/home/travis/.gimme/versions/go1.4.2.linux.amd64/bin:/usr/local/phantomjs/bin:./node_modules/.bin:/usr/local/maven-3.2.5/bin:/usr/local/clang-3.4/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
+fi
+
 echo -e
 echo -e "[$(date --rfc-3339 seconds)] - ${COLOR_BLUE}Runing tests...$COLOR_END"
 echo -e
