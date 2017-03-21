2acf2c50ae4fb4e17df0b11dcbcb54b9a7c5dde8
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-03-21 11:02:08 +0100

    replace # by @ in env vars when npm install

--- scripts/install-proxy.sh
+++ scripts/install-proxy.sh
@@ -25,7 +25,7 @@ pushd kuzzle-proxy > /dev/null
     echo -e "[$(date --rfc-3339 seconds)] - ${COLOR_BLUE}Override proxy common objects '${PROXY_COMMON_OBJECT_VERSION}' ...${COLOR_END}"
 
     npm uninstall kuzzle-common-object > /dev/null || true
-    npm install "${PROXY_COMMON_OBJECT_VERSION}" > /dev/null
+    npm install "${PROXY_COMMON_OBJECT_VERSION/#/@}" > /dev/null
   else
     echo -e "[$(date --rfc-3339 seconds)] - ${COLOR_YELLOW}Using default common objects embeded with proxy...${COLOR_END}"
   fi
@@ -34,7 +34,7 @@ pushd kuzzle-proxy > /dev/null
     echo -e "[$(date --rfc-3339 seconds)] - ${COLOR_BLUE}Override load balancer proxy version '${LB_PROXY_VERSION}' ...${COLOR_END}"
 
     npm uninstall kuzzle-proxy > /dev/null || true
-    npm install "${LB_PROXY_VERSION}" > /dev/null
+    npm install "${LB_PROXY_VERSION/#/@}" > /dev/null
   fi
 
   if [ ! -d "plugins/enabled" ]; then
