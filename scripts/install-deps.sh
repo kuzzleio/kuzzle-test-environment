c7fb16d9f8de7631edaa108c29bff692ef6851e9
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-04-04 15:57:34 +0200

    export CC and CXX version

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -9,7 +9,8 @@ COLOR_YELLOW="\e[33m"
 echo -e "[$(date --rfc-3339 seconds)] - ${COLOR_BLUE}Install debian packages dependencies...${COLOR_END}"
 
 apt-get update > /dev/null && \
-apt-get install -yqq --no-install-suggests --no-install-recommends --force-yes build-essential curl git gcc g++ gdb python openssl jq
+apt-get install -yq --no-install-suggests --no-install-recommends --force-yes build-essential curl git gcc-"$GCC_VERSION" g++-"$GCC_VERSION" gdb python openssl jq > /dev/null
+export CC="gcc-$GCC_VERSION" CXX="g++-$GCC_VERSION"
 
 # install nodejs in required version
 if [[ $(node --version) != "v$NODE_VERSION" ]]; then
