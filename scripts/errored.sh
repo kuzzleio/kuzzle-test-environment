1d6e8e544f3935b2a288400b9ea64844bcbf5476
STAFYNIAK Sacha <sstafyniak@kaliop.com>
2017-04-06 10:35:40 +0200

    fix compatibility matrix pm2 version

--- scripts/errored.sh
+++ scripts/errored.sh
@@ -16,7 +16,7 @@ echo -e "- ${COLOR_BLUE}proxy container logs:$COLOR_END"
 docker logs "proxy"
 
 echo -e "- ${COLOR_BLUE}proxy pm2 logs:$COLOR_END"
-docker exec -ti "proxy" bash -c "tail -n 1000 /root/.pm2/logs/*"
+docker exec -ti "proxy" bash -c "tail -n 100000 /root/.pm2/logs/*"
 
 for i in $(seq 1 ${KUZZLE_NODES:-1});
 do
@@ -27,7 +27,7 @@ do
   docker logs "kuzzle_${i}"
 
   echo -e "- ${COLOR_BLUE}kuzzle ${i} pm2 logs:$COLOR_END"
-  docker exec -ti "kuzzle_${i}" bash -c "tail -n 1000 /root/.pm2/logs/*"
+  docker exec -ti "kuzzle_${i}" bash -c "tail -n 100000 /root/.pm2/logs/*"
 done
 
 if [[ -e /tmp/sandbox/chaos_mode.log ]]; then
